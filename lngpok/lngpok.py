c64963cf73eecd505499c3d58d8286dc348d32dc
Blindshooter <alexanderruppelt@gmail.com>
2016-05-07 21:58:27 +0300

    Added files via upload

--- lngpok/lngpok.py
+++ lngpok/lngpok.py
@@ -6,7 +6,7 @@ def main():
     output_filename = "lngpok.out" if len(sys.argv) == 1 else sys.argv[2]
 
     array = read_input(input_filename)
-    result = solve(array)
+    result = solve1(array)
     write_output(output_filename, result)
 
 
@@ -52,6 +52,49 @@ def solve(a):
         k+=1
     return count+j
 
+def solve1(a):
+    if sum(a)==0:
+        return len(a)
+    w, j = csort(a, 0, max(a))
+    w1 = [w[i+1]-w[i]-1 for i in xrange(len(w)-1)]+[j+1]
+    return lca(w1, j, len(w1), max = len(w1))+1+j
+
+def lca(a, j,len_initial_arr, N = 0, max_len = 1, min=0, max=0):
+
+    if 2**N>len_initial_arr:
+        return max_len
+    else:
+        N+=1
+        #print ("N is", N)
+        M = max
+        m = min
+        #print ("Minmax are", min, max)
+        pivot = (M+m)//2
+        #print ("Pivot is ", pivot)
+
+        for i in xrange(len(a)-pivot+1):
+            flag = True
+            #print ("Now array", a[i:i+pivot], "compare to", j, "SUM IS",sum(a[i:i+pivot]) )
+            if sum(a[i:i+pivot])<=j:
+                flag = False
+
+                #print ('should be break', i)
+                break
+            else:
+                i+=1
+        if flag==False:
+            #print ("PIVOT >=max_len", pivot, max_len)
+            max_len = pivot
+            #print ("New max_len", max_len)
+            m = pivot
+            #print("I is", i)
+            new_arr = a
+            return lca(new_arr, j, len_initial_arr, N, max_len, m, M)
+        else:
+            M = pivot
+            return lca(a, j, len_initial_arr, N, max_len, m, M)
+
+
 
 def write_output(filename, total):
     with open(filename, "w") as output_file:
@@ -59,4 +102,5 @@ def write_output(filename, total):
 
 if __name__ == "__main__":
     main()
+
 
